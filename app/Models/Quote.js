ad27508dc141f258c191df02bb088774f7b9070d
Jonah <jonah.osito@icloud.com>
2023-02-11 17:43:59 -0700

    working on that hoverrr aaAAAA

---
+++ app/Models/Quote.js
@@ -0,0 +1,11 @@
+export class Quote{
+    constructor(data){
+        this.quote = data.content
+        this.author = data.author
+    }
+    get quoteTemp() {
+        return`
+        
+        `
+    }
+}
